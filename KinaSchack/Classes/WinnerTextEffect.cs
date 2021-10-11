7fd38a4b0344f63cfd70c5799373331e317d0384
Viktor C <vikkez_@hotmail.com>
2021-10-11 14:44:58 +0200

    comments

--- KinaSchack/Classes/WinnerTextEffect.cs
+++ KinaSchack/Classes/WinnerTextEffect.cs
@@ -15,6 +15,9 @@ using Windows.UI.Xaml.Media;
 
 namespace KinaSchack.Classes
 {
+    /// <summary>
+    /// Class <c>WinnerTextEffect</c> Applying an effect on winner text
+    /// </summary>
     class WinnerTextEffect
     {
         GaussianBlurEffect blur;
@@ -33,17 +36,21 @@ namespace KinaSchack.Classes
                 FontSize = _fontSize
             };
         }
-        
+        /// <summary>
+        /// Defines the effect. Can set the blur amount. Source is the text to apply the effect to
+        /// </summary>
         public void CreateEffect()
         {
             blur = new GaussianBlurEffect()
             {
                 Source = blur,
                 BlurAmount = 8.5f,
-                //BorderMode = EffectBorderMode.Soft
             };
         }
-
+        /// <summary>
+        /// Saving the text as list 
+        /// </summary>
+        /// <param name="sender"></param>
         public void SetupText(ICanvasAnimatedControl sender)
         {
             CanvasCommandList textCmdList = new CanvasCommandList(sender);
@@ -52,9 +59,7 @@ namespace KinaSchack.Classes
                 cmdlist.DrawText(_text, Scaling.GetScaledPoint(500, 100).x, Scaling.GetScaledPoint(500, 100).y, Colors.DeepPink, textFormat);
             }
             blur.Source = textCmdList;
-
         }
-
         public void ApplyEffectToText(ICanvasAnimatedControl sender)
         {
             CreateEffect();
@@ -66,11 +71,5 @@ namespace KinaSchack.Classes
             args.DrawingSession.DrawImage(blur);
             args.DrawingSession.DrawText(_text, Scaling.GetScaledPoint(500, 100).x, Scaling.GetScaledPoint(500, 100).y, Colors.DarkGray, textFormat);
         }
-        //public void DrawPlayerTurn(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
-        //{
-        //    ApplyEffectToText(sender);
-        //    args.DrawingSession.DrawImage(blur);
-        //    args.DrawingSession.DrawText(_player, 50, 50, Colors.DarkGray, textFormat);
-        //}
     }
 }
